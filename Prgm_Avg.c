b5b4614d4500d87f747128c061998e28162523c1
noelv1001 <59761118+noelv1001@users.noreply.github.com>
2020-01-11 15:13:56 +0530

    Create Prgm_Avg.c

---
+++ Prgm_Avg.c
@@ -0,0 +1,26 @@
+/* Program to find Average */
+
+#include<stdio.h>
+
+double getAverage(int arr[],int size);
+
+void main()
+{
+  int balance[5]={100,2,4,17,50};
+  double avg;
+  avg=getAverage(balance,5);
+  printf("Average=%d",avg);
+}
+
+double getAverage(int arr[],int size)
+{
+  int i;
+  double avg;
+  double sum;
+  for(i=0;i<size;i++)
+  {
+    sum+=arr[i];
+  }
+  avg=sum/size;
+  return avg;
+}
