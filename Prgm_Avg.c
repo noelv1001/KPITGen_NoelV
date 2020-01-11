1076a0447165ed15a6850a55967c5a6f0c2ec102
noelv1001 <59761118+noelv1001@users.noreply.github.com>
2020-01-11 15:44:08 +0530

    Update Prgm_Avg.c

--- Prgm_Avg.c
+++ Prgm_Avg.c
@@ -9,14 +9,14 @@ void main()
   int balance[5]={100,2,4,17,50};
   double avg;
   avg=getAverage(balance,5);
-  printf("Average=%f",avg);
+  printf("Average=%d",avg);
 }
 
 double getAverage(int arr[],int size)
 {
   int i;
   double avg;
-  int sum;
+  double sum;
   for(i=0;i<size;i++)
   {
     sum+=arr[i];
